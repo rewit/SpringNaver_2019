73f3446f7be8cd2150f7bdcd836deb460181f73a
rewit <rewit@naver.com>
2019-08-07 11:28:45 +0900

    first

---
+++ src/main/java/com/biz/naver/config/NaverSecret_sample.java
@@ -0,0 +1,11 @@
+package com.biz.naver.config;
+
+public class NaverSecret_sample {
+	
+	public final static String NaverClientId = "g1Wynbpftn4dyKZ73Ky4";
+	
+	public final static String NaverClientSecret ="9qgBPayQgj";
+	
+	
+
+}
